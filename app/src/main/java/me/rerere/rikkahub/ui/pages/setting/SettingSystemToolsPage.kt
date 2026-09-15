202c6d1a0718e213f5693a1a3ae244dee11b6782
100000220284 <shexv220284@gmail.com>
2026-09-15 20:39:34 +0800

    fix(setting): import padding plus extension in system tools page

--- app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingSystemToolsPage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/pages/setting/SettingSystemToolsPage.kt
@@ -21,6 +21,7 @@ import me.rerere.rikkahub.data.ai.tools.local.LocalToolOption
 import me.rerere.rikkahub.ui.components.nav.BackButton
 import me.rerere.rikkahub.ui.components.ui.CardGroup
 import me.rerere.rikkahub.ui.theme.CustomColors
+import me.rerere.rikkahub.utils.plus
 import org.koin.androidx.compose.koinViewModel
 
 private data class SystemToolRow(
